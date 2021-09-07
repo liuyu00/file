f2aca060ea97ef17dfe1a5b9ec3cf4baebb3c9c5
liuyu00 <liuyu009x@163.com>
2021-09-07 10:45:16 +0800

    初始仓库代码

---
+++ caoncat.js
@@ -0,0 +1,25 @@
+const concat = (answers) => {
+  const files = fs.readdirSync(process.cwd())
+  const concatFiles = files.filter((item) => {
+    if (item.endsWith('.' + type)) {
+      return true
+    }
+  })
+
+  const content = concatFiles.reduce((content, item, index) => {
+    const fileContent = fs.readFileSync( process.cwd() + '/' + item )
+    // console.log(content, fileContent.toString())
+    return `
+      ${content}
+      \/\/ ${item}
+      ${fileContent.toString()}
+    `
+  }, '')
+
+  // console.log(content)
+  // const content = 'sadasdasdasd'
+  // console.log(`./${answers.name}.${type}`)
+  fs.writeFileSync(`./${answers.name}.${type}`, content)
+}
+
+module.exports = concat
